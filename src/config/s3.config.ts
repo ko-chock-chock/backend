5a8b278712504d27be0d713ec7c03c29c4cb9957
kimsudang <0ksuyeon0@gmail.com>
2025-01-14 10:37:57 +0900

    fix: s3 경로 이상문제 해결

--- src/config/s3.config.ts
+++ src/config/s3.config.ts
@@ -13,13 +13,6 @@ export class S3ConfigService {
     this.bucketName = this.configService.get<string>('AWS_S3_BUCKET_NAME');
     this.region = 'ap-northeast-2';
 
-    // 만약 환경변수에서 region을 못 가져왔거나 문자열이 아닌 경우, fallback
-    if (!this.region || this.region.trim() === '') {
-      // fallback 예시: ap-northeast-2
-      console.warn('[S3ConfigService] AWS_REGION이 유효하지 않아, "ap-northeast-2"로 fallback합니다.');
-      this.region = 'ap-northeast-2'; // 원하는 기본값
-    }
-
     if (!this.bucketName || this.bucketName.trim() === '') {
       throw new Error('[S3ConfigService] S3 Bucket Name이 .env에 설정되지 않았습니다.');
     }
@@ -31,13 +24,22 @@ export class S3ConfigService {
       );
     }
 
-    // AWS SDK 클라이언트 생성
+    // AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY도 .env에서 가져오되
+    // 값을 못 찾으면 Error나 원하는 처리
+    const accessKeyId = this.configService.get<string>('AWS_ACCESS_KEY_ID');
+    const secretAccessKey = this.configService.get<string>('AWS_SECRET_ACCESS_KEY');
+
+    if (!accessKeyId || !secretAccessKey) {
+      throw new Error('[S3ConfigService] AWS 자격 증명(accessKey, secretKey)이 설정되지 않았습니다.');
+    }
+
+    /// S3Client 생성
     this.s3Client = new S3Client({
+      region: this.region, // 직접 하드코딩
       credentials: {
-        accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: this.configService.get<string>('AWS_SECRET_ACCESS_KEY'),
+        accessKeyId: accessKeyId,
+        secretAccessKey: secretAccessKey,
       },
-      region: this.region,
     });
 
     // 디버깅 로그
