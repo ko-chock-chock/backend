6ee2aa990e646b1f84fdf287f5bcd932b18622f9
kimsudang <0ksuyeon0@gmail.com>
2025-01-16 05:01:38 +0900

    fix: 유효하지 않은 accessToken 수정

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -31,7 +31,7 @@ export class AuthService {
       throw new UnauthorizedException('이메일 또는 비밀번호가 잘못되었습니다.');
     }
 
-    const payload = { user_id: user.user_id, mail: user.mail };
+    const payload = { sub: user.user_id, mail: user.mail };
     const accessToken = this.jwtService.sign(payload, {
       expiresIn: process.env.JWT_EXPIRES_IN || '60m',
     });
